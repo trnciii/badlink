370dc1966a0ae66846d50c80c7b85d87bcedd06e
trnciii <doityan223@gmail.com>
2022-07-13 00:23:16 +0900

    include main.py in __main__.py

--- lnman/__main__.py
+++ lnman/__main__.py
@@ -1,3 +1,36 @@
-from .main import main
+import sys
+from . import core
 
-main()
+
+def main():
+	ftable = {
+		'get': core.get,
+		'set': core.set,
+
+		'cat': core.cat,
+		'file': core.file,
+
+		'init': core.init,
+		'deinit': core.deinit,
+
+		'list': core.list_packages,
+		'show': core.show,
+		'lsdir': core.ls_pretty,
+
+		'install': core.install,
+		'add': core.add,
+		'remove': core.remove
+	}
+
+
+	argv = sys.argv
+
+	if len(argv)>1 and argv[1] in ftable.keys():
+		f, *args = argv[1:]
+		ret = ftable[f](*args)
+		if ret: print(ret)
+	else:
+		print('nothing to do')
+
+if __name__ == '__main__':
+	main()
--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
 	license='MIT',
 	packages=find_packages(),
 	entry_points={
-		'console_scripts': ['lnman = lnman.main:main']
+		'console_scripts': ['lnman = lnman.__main__:main']
 	}
 )
