aef550719a4c08a96caa23ae5be96fea7fa14b20
trnciii <doityan223@gmail.com>
2022-09-24 18:32:29 +0900

    use argparse

--- lnman/__main__.py
+++ lnman/__main__.py
@@ -1,36 +1,58 @@
-import sys
 from . import core
-
+import argparse
 
 def main():
-	ftable = {
-		'file': lambda:print(core.path_config),
+	parser = argparse.ArgumentParser()
+	sub = parser.add_subparsers()
 
-		'init': core.init,
-		'deinit': core.deinit,
+	sub.add_parser('file').set_defaults(handler=lambda _:print(core.path_config))
 
-		'list': core.list_packages,
-		'sites': core.list_sites,
-		'show': core.show,
-		'lsdir': core.list_directory,
+	p = sub.add_parser('init')
+	p.add_argument('key')
+	p.add_argument('path')
+	p.set_defaults(handler=lambda args:core.init(args.key, args.path))
 
-		'install': core.install,
-		'remove': core.remove,
-		'register': core.register,
-		'unregister': core.unregister,
+	p = sub.add_parser('deinit')
+	p.add_argument('key')
+	p.set_defaults(handler=lambda args:core.deinit(args.key))
 
-		'--version': lambda:print(core.version_string),
-		'url': lambda:print('https://github.com/trnciii/lnman')
-	}
+	sub.add_parser('list').set_defaults(handler=lambda _:core.list_packages())
+
+	sub.add_parser('sites').set_defaults(handler=lambda _:core.list_sites())
+
+	p = sub.add_parser('show')
+	p.add_argument('key')
+	p.set_defaults(handler=lambda args:core.show(args.key))
+
+	p = sub.add_parser('lsdir')
+	p.add_argument('key')
+	p.set_defaults(handler=lambda args:core.list_directory(args.key))
+
+	p = sub.add_parser('install')
+	p.add_argument('dst')
+	p.add_argument('src')
+	p.set_defaults(handler=lambda args:core.install(args.dst, args.src))
+
+	p = sub.add_parser('remove')
+	p.add_argument('key')
+	p.set_defaults(handler=lambda args:core.remove(args.key))
+
+	p = sub.add_parser('register')
+	p.add_argument('key')
+	p.set_defaults(handler=lambda args:core.register(args.key))
+
+	p = sub.add_parser('unregister')
+	p.add_argument('key')
+	p.set_defaults(handler=lambda args:core.unregister(args.key))
+
+	sub.add_parser('version').set_defaults(handler=lambda _:print(core.version_string))
+
+	sub.add_parser('url').set_defaults(handler=lambda _:print('https://github.com/trnciii/lnman'))
 
 
-	argv = sys.argv
-
-	if len(argv)>1 and argv[1] in ftable.keys():
-		f, *args = argv[1:]
-		ftable[f](*args)
-	else:
-		print('nothing to do')
+	args = parser.parse_args()
+	if hasattr(args, 'handler'):
+		args.handler(args)
 
 if __name__ == '__main__':
 	main()
